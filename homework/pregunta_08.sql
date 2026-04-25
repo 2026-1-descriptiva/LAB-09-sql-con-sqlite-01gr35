2ec48d57ab7d27d03dd26860ed64a2479b046827
53rg10 <semontoyape@unal.edu.co>
2026-04-24 21:32:07 -0500

    save

--- homework/pregunta_08.sql
+++ homework/pregunta_08.sql
@@ -41,3 +41,9 @@
 --
 --  >>> Escriba su codigo a partir de este punto <<<
 --
+
+SELECT strftime('%Y', c23) AS 'strftime(''%Y'', c23)', 
+       ROUND(AVG(c21), 2) AS 'avg(c21)'
+FROM tbl2
+GROUP BY strftime('%Y', c23)
+ORDER BY strftime('%Y', c23);
